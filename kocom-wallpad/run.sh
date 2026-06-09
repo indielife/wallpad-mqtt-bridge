a72ed9af04ba0e0ccc5d826576a160233909c92f
Hyunil Kim <indielife@hotmail.com>
2026-06-10 01:51:30 +0900

    refactor: rs485.py를 main.py로 변경 및 Dockerfile 재구성
    - 진입점 파이썬 스크립트 이름을 rs485.py에서 main.py로 변경
    - 변경된 스크립트 명칭 반영 및 빌드 레이어 캐시 최적화를 위해
      Dockerfile 구조 업데이트

--- kocom-wallpad/run.sh
+++ kocom-wallpad/run.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 SHARE_DIR=/share/kocom
+mkdir -p $SHARE_DIR
+cp -u /*.py $SHARE_DIR
 
-if [ ! -f $SHARE_DIR/rs485.py ]; then
-	mkdir $SHARE_DIR
-	mv /rs485.py $SHARE_DIR
-fi
 /makeconf.sh
 
 echo "[Info] Run Wallpad Controller"
 cd $SHARE_DIR
-python3 $SHARE_DIR/rs485.py
+python3 $SHARE_DIR/main.py
 
 # for dev
-while true; do echo "still live"; sleep 100; done
+# while true; do echo "still live"; sleep 100; done
