4e6dfacac0dd04560fcfc15f609de5ac456cbdc4
Hyunil Kim <indielife@hotmail.com>
2026-07-02 23:49:14 +0900

    🧹 chore(mqtt): bridge config 토픽 상수 추가
    - wallpad/bridge/config/* 서브커맨드들의 완성된 토픽 문자열을
      constants.py에 상수로 정의 (라우팅이 exact-match 기반으로 바뀌므로
      세그먼트 조각이 아닌 전체 토픽 문자열을 상수화)
    
    Ref #54

--- src/wallpad/mqtt/__init__.py
+++ src/wallpad/mqtt/__init__.py
@@ -1,21 +1,35 @@
 from wallpad.mqtt.client import MqttClient
 from wallpad.mqtt.config import MqttConfig
 from wallpad.mqtt.constants import (
+    BRIDGE_TOPIC_PREFIX,
     HA_CLIMATE,
     HA_FAN,
     HA_LIGHT,
     HA_PREFIX,
     HA_SENSOR,
     HA_SWITCH,
+    TOPIC_BRIDGE_CHECKSUM,
+    TOPIC_BRIDGE_LOG_LEVEL,
+    TOPIC_BRIDGE_PACKET,
+    TOPIC_BRIDGE_REMOVE,
+    TOPIC_BRIDGE_RESTART,
+    TOPIC_BRIDGE_SCAN,
 )
 
 __all__ = [
+    "BRIDGE_TOPIC_PREFIX",
     "HA_CLIMATE",
     "HA_FAN",
     "HA_LIGHT",
     "HA_PREFIX",
     "HA_SENSOR",
     "HA_SWITCH",
+    "TOPIC_BRIDGE_CHECKSUM",
+    "TOPIC_BRIDGE_LOG_LEVEL",
+    "TOPIC_BRIDGE_PACKET",
+    "TOPIC_BRIDGE_REMOVE",
+    "TOPIC_BRIDGE_RESTART",
+    "TOPIC_BRIDGE_SCAN",
     "MqttClient",
     "MqttConfig",
 ]
